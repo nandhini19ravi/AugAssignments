e39a41e3b14bc18b1a1ef0f7b7dc061944bda6f9
nandhini19ravi <95162337+nandhini19ravi@users.noreply.github.com>
2022-08-11 20:46:04 +0530

    week1/day2
    week1/day2

---
+++ week1/week1/day2/Calculator.java
@@ -0,0 +1,26 @@
+package week1.day2;
+
+public class Calculator {
+	public int add (int a1, int a2, int a3) {
+		int sum = a1+a2+a3;
+		return sum;
+	}
+	public int sub(int s1, int s2) {
+		int minus= s1-s2;
+		return minus;
+	}
+	public double mul(double m1, double m2) {
+		double multiply = m1*m2;
+		return multiply;
+	}
+	public float div (float d1, float d2) {
+		float divide = d1/d2;
+		return divide;
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ week1/week1/day2/OddNumbers.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class OddNumbers {
+
+	public static void main(String[] args) {
+		for(int i= 51; i<=77; i++) {
+			if (i%2!=0) {
+				System.out.println("Odd Number from 51 to 77 is "+i);
+			}
+		}
+
+	}
+
+}
